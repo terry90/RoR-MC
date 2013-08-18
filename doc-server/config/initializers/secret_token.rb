bc13e781a9dbe021a95358f99aa034061bbfa37c
Doc <terry.raimondo@gmail.com>
2013-08-18 13:17:06 -0700

    [INIT] Initialization of rails project

---
+++ doc-server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DocServer::Application.config.secret_token = 'c157a8b4312b6e1df35ea4efbf4fd8ca2604f41e5027a514b837fd47c751d636b35ed8498f003372a9cc8ff1473d579f9a65f3b8cfe42aeafaf644e17a6e2761'
